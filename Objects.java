eb70c86298c744f45feea4bda094f6335826a48e
zainabkamran-gif <zainab.kamran@emu.edu>
2026-04-29 14:28:27 -0400

    Add files via upload

---
+++ Objects.java
@@ -0,0 +1,13 @@
+public class Objects {
+    float x;
+    float y;
+    float size;
+
+    public Objects(float x, float y) {
+    this.x = x;
+    this.y = y;
+    this.size = 20;
+    }                        
+
+    public void display(PApplet p) {}   // not necessarily needed
+}                            

d2fdf179814d31994cc1a2396ff0a02a16334108
zainabkamran-gif <zainab.kamran@emu.edu>
2026-04-29 14:48:11 -0400

    Update Days.java

--- Days.java
+++ Days.java
@@ -11,6 +11,7 @@ public void increment(PApplet p) {
 if (p.keyPressed && key == 'z' && p.frameCount%2==0) { // increases day by 1 if key pressed is z and if the user presses the key NOT MORE THAN TWICE
 d++;
 }
+}
 public void display(PApplet p) {
     p.fill(0);
     p.textSize(20);
@@ -18,4 +19,4 @@ public void display(PApplet p) {
     p.text("Day: " + d, 10, 30);
 }
 }
-}
+

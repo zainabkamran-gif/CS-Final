ca39059ce5534c0012dc6af8261e125867da721d
zainabkamran-gif <zainab.kamran@emu.edu>
2026-04-29 14:24:22 -0400

    Add files via upload

---
+++ Days.java
@@ -0,0 +1,21 @@
+public class Days extends Objects {
+int d;
+
+// constructor
+public Days() {
+super(0,0);
+d = 1;
+}
+
+public void increment(PApplet p) { // look into this
+if (p.keyPressed && key == 'z' && p.frameCount%2==0) { // increases day by 1 if key pressed is z and if the user presses the key NOT MORE THAN TWICE
+d++;
+}
+public void display(PApplet p) {
+    p.fill(0);
+    p.textSize(20);
+    p.textAlign(PApplet.LEFT);
+    p.text("Day: " + d, 10, 30);
+}
+}
+}

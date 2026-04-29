feb8b5bcd8a6c0ef36de91dc0f41e03193f8c1da
zainabkamran-gif <zainab.kamran@emu.edu>
2026-04-29 15:08:30 -0400

    Update RestInPeace.java

--- RestInPeace.java
+++ RestInPeace.java
@@ -8,14 +8,14 @@ chicDeathCount=0;
 duckDeathCount=0;
 }
 // if x is pressed, a chicken dies
-public boolean cdeath(PApplet p) {
+public void cdeath(PApplet p) {
 if (p.keyPressed && p.key=='x') {
 chicDeathCount++;
 }
 }
 
 // if y is pressed, a duck dies
-public boolean ddeath(PApplet p) {
+public void ddeath(PApplet p) {
 if (p.keyPressed && p.key=='y') {
 duckDeathCount++;
 }
@@ -30,6 +30,6 @@ p.text("Chicken Death Count: " + chicDeathCount,10,50);
 p.fill(255,0,0);
 p.textSize(30);
 p.textAlign(PApplet.LEFT);
-p.text("Duck Death Count: " + duckDeathCount,10,50);
+p.text("Duck Death Count: " + duckDeathCount,10,90);
 }
 }

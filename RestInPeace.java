64877cfea461c52096ab22b92c2f134808fa56bd
zainabkamran-gif <zainab.kamran@emu.edu>
2026-04-29 15:02:34 -0400

    Update RestInPeace.java

--- RestInPeace.java
+++ RestInPeace.java
@@ -1,21 +1,35 @@
 public class RestInPeace extends Objects {
-int deathCount;
+int chicDeathCount;
+int duckDeathCount;
 // constructor
 public RestInPeace() {
 super(0,0);
-deathCount=0;
+chicDeathCount=0;
+duckDeathCount=0;
 }
-// if x is pressed, a fowl dies
-public boolean death(PApplet p) {
+// if x is pressed, a chicken dies
+public boolean cdeath(PApplet p) {
 if (p.keyPressed && p.key=='x') {
-deathCount++;
+chicDeathCount++;
 }
 }
+
+// if y is pressed, a duck dies
+public boolean ddeath(PApplet p) {
+if (p.keyPressed && p.key=='y') {
+duckDeathCount++;
+}
+}
+
 // displaying the death count
 public void display(PApplet p) {
 p.fill(255,0,0);
 p.textSize(30);
 p.textAlign(PApplet.LEFT);
-p.text("Death Count: " + deathCount,10,50);
+p.text("Chicken Death Count: " + chicDeathCount,10,50);
+p.fill(255,0,0);
+p.textSize(30);
+p.textAlign(PApplet.LEFT);
+p.text("Duck Death Count: " + duckDeathCount,10,50);
 }
 }

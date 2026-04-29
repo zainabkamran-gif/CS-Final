8d2c4880177f4d807fa62c9ce48af14382dc495a
zainabkamran-gif <zainab.kamran@emu.edu>
2026-04-29 15:09:54 -0400

    Update TotalEggs.java

--- TotalEggs.java
+++ TotalEggs.java
@@ -2,7 +2,7 @@ public class TotalEggs extends Objects {
 int total;
 // constructor
 public TotalEggs() {
-super(0.0);
+super(0,0);
 total=0;
 }
 public void count(Fowls fowl, RestInPeace rip) {
@@ -13,6 +13,6 @@ public void display(PApplet p) {
  p.fill(255,0,0);
  p.textSize(30);
  p.textAlign(PApplet.LEFT);
- p.text("Total Eggs: " + total, 10, 90);
+ p.text("Total Eggs: " + total, 10, 130);
 }
 }
